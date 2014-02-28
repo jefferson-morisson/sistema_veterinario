224aeec3611e910d95f40ceed72e034949bace33
Jefferson Morisson <morisson.rj@gmail.com>
2014-02-28 13:19:36 -0300

    Criando classe Animal

---
+++ animal.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+class Animal():
+
+	def __init__(self, codAnimal, nome, sexo, especie):
+		self.codAnimal = codAnimal
+		self.nome = nome
+		self.sexo = sexo
+		self.especie = especie
