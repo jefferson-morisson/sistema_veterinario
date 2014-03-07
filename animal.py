9b10e623824df034e89218c3a03f9841baf69f1d
Rayssa Bugine <rayssa.rbugine@gmail.com>
2014-03-07 20:29:35 -0300

    Adicionando exceção para vereficar se Especie é uma classe

--- animal.py
+++ animal.py
@@ -6,4 +6,8 @@ class Animal():
 		self.codAnimal = codAnimal
 		self.nome = nome
 		self.sexo = sexo
-		self.especie = especie
+		try: 
+			especie.__class__ == Especie
+			self.especie = especie
+		except:
+			raise TypeError('especie tem que ser um objeto do tipo Especie')
