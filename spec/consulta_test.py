476b9415aa4d78f0e39484cc8d893a8ce85dbd3d
Rayssa Bugine <rayssa.rbugine@gmail.com>
2014-03-08 19:05:51 -0300

    Teste da função fazer update em consulta

--- spec/consulta_test.py
+++ spec/consulta_test.py
@@ -19,3 +19,19 @@ class ConsultaTest(unittest.TestCase):
 		consulta.veterinario |should| equal_to(veterinario.nome)
 		consulta.animal |should| equal_to(animal.nome)
 		consulta.resumoConsulta |should| equal_to("Resumo")
+
+	def test_fazer_update(self):
+		veterinario = Veterinario(5, "Marcelo", "102102121")
+		animal = Animal(2, "Cachorro", "Macho", "Mamífero")
+		consulta = Consulta(1, "12/12/2012", veterinario.nome, animal.nome, "Resumo")
+		consulta.data |should| equal_to("12/12/2012")
+
+		veterinario_novo = Veterinario(7, "Lucas", "444444")
+		animal_novo = Animal(78, "Gato", "Macho", "Mamífero")
+
+		consulta.fazer_update("26/10/2013", veterinario_novo.nome, animal_novo.nome, "Resumo novo")
+		
+		consulta.data |should| equal_to("26/10/2013")
+		consulta.veterinario |should| equal_to(veterinario_novo.nome)
+		consulta.animal |should| equal_to(animal_novo.nome)
+		consulta.resumoConsulta |should| equal_to("Resumo novo")
