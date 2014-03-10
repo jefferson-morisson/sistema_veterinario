6c93ad06c8187ab7061fcb6cff62e62b5d20e2e4
Jefferson Morisson <morisson.rj@gmail.com>
2014-03-10 17:08:56 -0300

    Criando função de fazer update em cliente

--- cliente.py
+++ cliente.py
@@ -6,3 +6,8 @@ class Cliente():
 		self.nome = nome
 		self.endereco = endereco
 		self.telefone = telefone
+
+	def fazer_update(self, nome_novo, endereco_novo, telefone_novo):
+		self.nome = nome_novo
+		self.endereco = endereco_novo
+		self.telefone = telefone_novo
