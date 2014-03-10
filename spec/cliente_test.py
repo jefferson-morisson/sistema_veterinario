e122ac35eb1e98efdcbdd956ae3c5c8badcda086
Jefferson Morisson <morisson.rj@gmail.com>
2014-03-10 17:06:33 -0300

    Criando teste para fazer update em cliente

--- spec/cliente_test.py
+++ spec/cliente_test.py
@@ -12,3 +12,11 @@ class ClienteTest(unittest.TestCase):
 		cliente.nome |should| equal_to("João")
 		cliente.endereco |should| equal_to("Avenida 28 de março")
 		cliente.telefone |should| equal_to("9 9888-9889")
+
+	def test_fazer_update(self):
+		cliente = Cliente(1, "João", "Avenida 28 de março", "9 9888-9889")
+		cliente.fazer_update("Gustavo", "Princesa Isabel", "9 9444-9894")
+		cliente.codCliente |should| equal_to(1)
+		cliente.nome |should| equal_to("Gustavo")
+		cliente.endereco |should| equal_to("Princesa Isabel")
+		cliente.telefone |should| equal_to("9 9444-9894")
