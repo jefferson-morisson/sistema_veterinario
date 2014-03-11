c609c7d3f92e8c5b593dfd5eb8efe4870ce85a0e
Jefferson Morisson <morisson.rj@gmail.com>
2014-03-11 11:55:56 -0300

    Criando função de inserir lista de animais em Cliente

--- cliente.py
+++ cliente.py
@@ -1,13 +1,17 @@
 #coding: utf-8
 class Cliente():
 
-	def __init__(self, codCliente, nome, endereco, telefone):
+	def __init__(self, codCliente, nome, endereco, telefone, lista_animais =[]):
 		self.codCliente = codCliente
 		self.nome = nome
 		self.endereco = endereco
 		self.telefone = telefone
+		self.lista_animais =[]
 
 	def fazer_update(self, nome_novo, endereco_novo, telefone_novo):
 		self.nome = nome_novo
 		self.endereco = endereco_novo
 		self.telefone = telefone_novo
+
+	def inserir_lista_animais(self, animal):
+		self.lista_animais.append(animal)
