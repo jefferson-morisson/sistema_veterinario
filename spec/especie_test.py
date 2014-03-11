3186710d2a29d930afe9e2e1dd25853f087db755
Jefferson Morisson <morisson.rj@gmail.com>
2014-03-11 18:19:25 -0300

    Criando função de inserir lista de animais em Especie

--- spec/especie_test.py
+++ spec/especie_test.py
@@ -2,6 +2,7 @@
 import unittest
 from should_dsl import should
 from sistema_veterinario.especie import Especie
+from sistema_veterinario.animal import Animal
 
 #Testes com a classe Especie
 class EspecieTest(unittest.TestCase):
@@ -15,3 +16,14 @@ class EspecieTest(unittest.TestCase):
 		especie = Especie(1, "Mamífero")
 		especie.fazer_update("Réptil")
 		especie.nome |should| equal_to("Réptil")
+
+	def test_inserir_lista_animais(self):
+		especie = Especie(20, "Mamífero")
+		animal = Animal(10, "Cachorro", "Macho", especie.nome)   
+		animal2 = Animal(30, "Boi", "Macho", especie.nome)      
+
+		especie.inserir_lista_animais(animal)
+		especie.inserir_lista_animais(animal2)
+
+		(animal in especie.lista_animais) |should| equal_to(True) 
+		(animal2 in especie.lista_animais) |should| equal_to(True)
