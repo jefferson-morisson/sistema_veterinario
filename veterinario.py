bca56a622ff09a2ec57bc4fa8120b29545662a61
Rayssa Bugine <rayssa.rbugine@gmail.com>
2014-03-11 18:35:35 -0300

    Função para inserir lista de consultas em Veterinario

--- veterinario.py
+++ veterinario.py
@@ -1,11 +1,15 @@
 #coding: utf-8
 class Veterinario():
 
-	def __init__(self, codVeterinario, nome, crmv):
+	def __init__(self, codVeterinario, nome, crmv, lista_consultas=[]):
 		self.codVeterinario = codVeterinario
 		self.nome = nome
 		self.crmv = crmv
+		self.lista_consultas =[]
 
 	def fazer_update(self, nome_novo, crmv_novo):
 		self.nome = nome_novo
 		self.crmv = crmv_novo
+
+	def inserir_lista_consultas(self, consulta):
+		self.lista_consultas.append(consulta)
