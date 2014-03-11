03e9f81ab398ac374b9820ced72bbbb6f7b5c1f1
Rayssa Bugine <rayssa.rbugine@gmail.com>
2014-03-11 18:34:46 -0300

    Teste da função inserir lista de Consultas do Veterinario

--- spec/veterinario_test.py
+++ spec/veterinario_test.py
@@ -2,6 +2,9 @@
 import unittest
 from should_dsl import should
 from sistema_veterinario.veterinario import Veterinario
+from sistema_veterinario.especie import Especie
+from sistema_veterinario.consulta import Consulta
+from sistema_veterinario.animal import Animal
 
 #Testes com a classe Veterinário
 class VeterinarioTest(unittest.TestCase):
@@ -18,3 +21,16 @@ class VeterinarioTest(unittest.TestCase):
 		veterinario.nome |should| equal_to("Carlos")
 		veterinario.crmv |should| equal_to("454572")
 		
+	def test_inserir_lista_consultas(self):
+		especie = Especie(20, "Mamífero")
+		animal = Animal(10, "Cachorro", "Macho", especie.nome)   
+		veterinario = Veterinario(5, "Marcelo", "102102121")
+
+		consulta = Consulta(1, "12/12/2012", veterinario.nome, animal.nome, "Resumo")
+		consulta2 = Consulta(2, "24/12/2012", veterinario.nome, animal.nome, "Resumo 2")
+		
+		veterinario.inserir_lista_consultas(consulta)
+		veterinario.inserir_lista_consultas(consulta2)
+
+		(consulta in veterinario.lista_consultas) |should| equal_to(True) 
+		(consulta2 in veterinario.lista_consultas) |should| equal_to(True) 
