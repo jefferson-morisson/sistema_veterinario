8c948e7cc348ca84d7d38cabd655e5bac0ab0adc
Jefferson Morisson <morisson.rj@gmail.com>
2014-03-11 18:21:33 -0300

    Criando função de inserir lista de animais em Especie

--- especie.py
+++ especie.py
@@ -1,10 +1,13 @@
 #coding: utf-8
 class Especie():
 
-	def __init__(self, codEspecie, nome):
+	def __init__(self, codEspecie, nome, lista_animais = []):
 		self.codEspecie = codEspecie
 		self.nome = nome
-
+		self.lista_animais = []
 
 	def fazer_update(self, nome_novo):
 		self.nome = nome_novo
+
+	def inserir_lista_animais(self, animal):
+		self.lista_animais.append(animal)
